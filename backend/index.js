ff95ec6e84494e4c9de8e77a2f534c0765ca967f
Rajat Kumar Jangid <jangidrajat105@gmail.com>
2024-02-14 13:03:08 +0530

    commited

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const { connection } = require("./config/db");
 const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/note.routes");
+const { cors } = require("cors");
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
+app.use(cors);
 
 
 app.get("/", (req, res)=>{
