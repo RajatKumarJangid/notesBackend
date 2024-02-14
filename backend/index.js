a3a511faca0858666a75d7da2deed95703ae58e7
Rajat Kumar Jangid <jangidrajat105@gmail.com>
2024-02-14 13:58:03 +0530

    commited

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const { cors } = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
 
